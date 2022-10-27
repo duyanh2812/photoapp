bc0f3d9450d4df84dd31c72f79e0ed31413dd9bb
Duy Anh <ndanh4@cmcglobal.vn>
2022-10-27 09:45:20 +0700

    Add log zipkin, sleuth

--- PhotoAppApiUsers/src/main/java/com/duyanh/photoapp/api/users/service/UsersServiceImpl.java
+++ PhotoAppApiUsers/src/main/java/com/duyanh/photoapp/api/users/service/UsersServiceImpl.java
@@ -25,90 +25,88 @@ import com.duyanh.photoapp.api.users.ui.model.AlbumResponseModel;
 
 @Service
 public class UsersServiceImpl implements UsersService {
-	
+
 	UsersRepository usersRepository;
 	BCryptPasswordEncoder bCryptPasswordEncoder;
-	//RestTemplate restTemplate;
+	// RestTemplate restTemplate;
 	Environment environment;
 	AlbumsServiceClient albumsServiceClient;
-	
+
 	RestTemplate restTemplate;
-	
+
 	Logger logger = LoggerFactory.getLogger(this.getClass());
-	
+
 	@Autowired
-	public UsersServiceImpl(UsersRepository usersRepository, 
-			BCryptPasswordEncoder bCryptPasswordEncoder,
-			Environment environment, AlbumsServiceClient albumsServiceClient)
-	{
+	public UsersServiceImpl(UsersRepository usersRepository, BCryptPasswordEncoder bCryptPasswordEncoder,
+			Environment environment, AlbumsServiceClient albumsServiceClient) {
 		this.usersRepository = usersRepository;
 		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 		this.environment = environment;
 		this.albumsServiceClient = albumsServiceClient;
 	}
- 
+
 	@Override
 	public UserDto createUser(UserDto userDetails) {
 		// TODO Auto-generated method stub
-		
+
 		userDetails.setUserId(UUID.randomUUID().toString());
 		userDetails.setEncryptedPassword(bCryptPasswordEncoder.encode(userDetails.getPassword()));
-		
-		ModelMapper modelMapper = new ModelMapper(); 
+
+		ModelMapper modelMapper = new ModelMapper();
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-		
+
 		UserEntity userEntity = modelMapper.map(userDetails, UserEntity.class);
 
 		usersRepository.save(userEntity);
-		
+
 		UserDto returnValue = modelMapper.map(userEntity, UserDto.class);
- 
+
 		return returnValue;
 	}
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		UserEntity userEntity = usersRepository.findByEmail(username);
-		
-		if(userEntity == null) throw new UsernameNotFoundException(username);	
-		
-		return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), true, true, true, true, new ArrayList<>());
+
+		if (userEntity == null)
+			throw new UsernameNotFoundException(username);
+
+		return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), true, true, true, true,
+				new ArrayList<>());
 	}
 
 	@Override
 	public UserDto getUserDetailsByEmail(String email) {
 		// TODO Auto-generated method stub
 		UserEntity userEntity = usersRepository.findByEmail(email);
-		
-		if(userEntity == null) throw new UsernameNotFoundException(email);
-		
-		
+
+		if (userEntity == null)
+			throw new UsernameNotFoundException(email);
+
 		return new ModelMapper().map(userEntity, UserDto.class);
 	}
 
 	@Override
 	public UserDto getUserByUserId(String userId) {
 		// TODO Auto-generated method stub
-		 UserEntity userEntity = usersRepository.findByUserId(userId);     
-	        if(userEntity == null) throw new UsernameNotFoundException("User not found");
-	        
-	        UserDto userDto = new ModelMapper().map(userEntity, UserDto.class);
-	        
-	        
+		UserEntity userEntity = usersRepository.findByUserId(userId);
+		if (userEntity == null)
+			throw new UsernameNotFoundException("User not found");
+
+		UserDto userDto = new ModelMapper().map(userEntity, UserDto.class);
+
 //	        String albumsUrl = String.format(environment.getProperty("albums.url"), userId);
 //	        
 //	        ResponseEntity<List<AlbumResponseModel>> albumsListResponse = restTemplate.exchange(albumsUrl, HttpMethod.GET, null, new ParameterizedTypeReference<List<AlbumResponseModel>>() {
 //	        });
 //	        List<AlbumResponseModel> albumsList = albumsListResponse.getBody(); 
-	        
-	       
-	        	logger.info("Before calling albums Microservice");
-		        List<AlbumResponseModel> albumsList = albumsServiceClient.getAlbums(userId);
-		        logger.info("After calling albums Microservice");
-		        userDto.setAlbums(albumsList);
-	        
-			return userDto;
+
+		logger.info("Before calling albums Microservice");
+		List<AlbumResponseModel> albumsList = albumsServiceClient.getAlbums(userId);
+		logger.info("After calling albums Microservice");
+		userDto.setAlbums(albumsList);
+
+		return userDto;
 	}
-	
 
 }
