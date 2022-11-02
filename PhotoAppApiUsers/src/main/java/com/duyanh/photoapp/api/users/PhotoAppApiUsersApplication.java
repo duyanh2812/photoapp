ab75a337f9f232a5a27c0ad38ab7c6b18d329694
Duy Anh <ndanh4@cmcglobal.vn>
2022-11-02 14:46:27 +0700

    update

--- PhotoAppApiUsers/src/main/java/com/duyanh/photoapp/api/users/PhotoAppApiUsersApplication.java
+++ PhotoAppApiUsers/src/main/java/com/duyanh/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -1,10 +1,13 @@
 package com.duyanh.photoapp.api.users;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import feign.Logger;
@@ -14,6 +17,8 @@ import feign.Logger;
 @EnableFeignClients
 public class PhotoAppApiUsersApplication {
 
+	@Autowired
+	Environment env;
 	public static void main(String[] args) {
 		SpringApplication.run(PhotoAppApiUsersApplication.class, args);
 	}
@@ -31,9 +36,38 @@ public class PhotoAppApiUsersApplication {
 //	}
 	
 	@Bean
+	@Profile("production")
+	Logger.Level feignProductionLoggerLevel()
+	{
+		return Logger.Level.NONE;
+	}
+	
+	@Bean
+	@Profile("!production")
 	Logger.Level feignDefaultLoggerLevel()
 	{
 		return Logger.Level.FULL;
+	}
+	
+	@Bean
+	@Profile("production")
+	public String createProductionBean() {
+		System.out.println("Production bean created" + env.getProperty("myapplication.environment"));
+		return "Production bean";
+	}
+	
+	@Bean
+	@Profile("!production")
+	public String createNotProductionBean() {
+		System.out.println("Not Production bean created" + env.getProperty("myapplication.environment"));
+		return "Not Production bean";
+	}
+	
+	@Bean
+	@Profile("default")
+	public String createDefaultBean() {
+		System.out.println("Development bean created" + env.getProperty("myapplication.environment"));
+		return "Production bean";
 	}
 
 }
